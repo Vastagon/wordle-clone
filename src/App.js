3d9ff9275104382676e8b41905c96ecc9e855513
Vastagon <jacob.yancey29@yahoo.com>
2022-03-27 03:22:41 -0500

    Added partial correctness and complete wrong boxes. Need to add a visual keyboard for right and wrong letters

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import PrevWordRow from './components/PrevWordRow';
 import {useState} from "react"
 
 function App() {
+  const [wrongLetters, setWrongLetters] = useState([])
   const [letterPositions, setLetterPositions] = useState([])
   const [previousTries, setPreviousTries] = useState([])
   const [heightCounter, setHeightCounter] = useState(0)
   const [currentWord, setCurrentWord] = useState(wordsList[Math.floor(Math.random() * wordsList.length)])
   const [textInput, setTextInput] = useState("")
-  const [colorArrays, setColorArrays] = useState([])
-  // colorArrays.length = 5
+  const [greenArrays, setgreenArrays] = useState([])
+  // greenArrays.length = 5
 
   console.log(currentWord)
 
@@ -38,32 +39,33 @@ function App() {
       if(currentWord.substring(i,i+1) == textInput.substring(i,i+1)){
         letterPositions.push(i)
       }
-      ///Checks if letter is in the word
-
-
-
-      ///Letter not in the word
     }
 
     ///Clears the inputs
     document.getElementById('text-input').value = ""
     setTextInput()
-    colorArrays.push([letterPositions])
+    greenArrays.push([letterPositions])
     setLetterPositions([])
   }
-  // console.log(colorArrays)
+
+  const wLetters = wrongLetters.map(prev => {
+    return <p>{prev}</p>
+  })
+
+  // console.log(greenArrays)
   return (
     <div className="App">
-      {heightCounter === 0 ? <WordRow  textInput={textInput}/> : <PrevWordRow colorArrays={colorArrays[0]} letterPositions={letterPositions} previousTries={previousTries[0]}/>}
-      {heightCounter === 1 ? <WordRow  textInput={textInput}/> : <PrevWordRow colorArrays={colorArrays[1]} letterPositions={letterPositions} previousTries={previousTries[1]}/>}
-      {heightCounter === 2 ? <WordRow  textInput={textInput}/> : <PrevWordRow colorArrays={colorArrays[2]} letterPositions={letterPositions} previousTries={previousTries[2]}/>}
-      {heightCounter === 3 ? <WordRow  textInput={textInput}/> : <PrevWordRow colorArrays={colorArrays[3]} letterPositions={letterPositions} previousTries={previousTries[3]}/>}
-      {heightCounter === 4 ? <WordRow  textInput={textInput}/> : <PrevWordRow colorArrays={colorArrays[4]} letterPositions={letterPositions} previousTries={previousTries[4]}/>}
-      {heightCounter === 5 ? <WordRow  textInput={textInput}/> : <PrevWordRow colorArrays={colorArrays[5]} letterPositions={letterPositions} previousTries={previousTries[5]}/>}
+      {heightCounter === 0 ? <WordRow  textInput={textInput}/> : <PrevWordRow setWrongLetters={setWrongLetters} wrongLetters={wrongLetters} currentWord={currentWord} greenArrays={greenArrays[0]} letterPositions={letterPositions} previousTries={previousTries[0]}/>}
+      {heightCounter === 1 ? <WordRow  textInput={textInput}/> : <PrevWordRow setWrongLetters={setWrongLetters} wrongLetters={wrongLetters} currentWord={currentWord} greenArrays={greenArrays[1]} letterPositions={letterPositions} previousTries={previousTries[1]}/>}
+      {heightCounter === 2 ? <WordRow  textInput={textInput}/> : <PrevWordRow setWrongLetters={setWrongLetters} wrongLetters={wrongLetters} currentWord={currentWord} greenArrays={greenArrays[2]} letterPositions={letterPositions} previousTries={previousTries[2]}/>}
+      {heightCounter === 3 ? <WordRow  textInput={textInput}/> : <PrevWordRow setWrongLetters={setWrongLetters} wrongLetters={wrongLetters} currentWord={currentWord} greenArrays={greenArrays[3]} letterPositions={letterPositions} previousTries={previousTries[3]}/>}
+      {heightCounter === 4 ? <WordRow  textInput={textInput}/> : <PrevWordRow setWrongLetters={setWrongLetters} wrongLetters={wrongLetters} currentWord={currentWord} greenArrays={greenArrays[4]} letterPositions={letterPositions} previousTries={previousTries[4]}/>}
+      {heightCounter === 5 ? <WordRow  textInput={textInput}/> : <PrevWordRow setWrongLetters={setWrongLetters} wrongLetters={wrongLetters} currentWord={currentWord} greenArrays={greenArrays[5]} letterPositions={letterPositions} previousTries={previousTries[5]}/>}
       <form onSubmit={handleFormSubmit}>
         <input onChange={handleTextInput} type="text" className="text-input" id="text-input" />
         <button type='submit'>Submit</button>
       </form>
+      {/* {wLetters} */}
     </div>
   );
 }
--- src/components/PrevWordRow.js
+++ src/components/PrevWordRow.js
@@ -1,49 +1,106 @@
 import {useState} from "react"
+import WordRow from "./WordRow"
 
-export default function PrevWordRow({colorArrays, letterPositions, previousTries}){
-    const [individualArray, setIndividualArray] = useState([])
-
-    if(colorArrays){
-        colorArrays = colorArrays[0]
-        // console.log(colorArrays)
+export default function PrevWordRow({setWrongLetters, wrongLetters, currentWord, greenArrays, previousTries}){
+    const yellowWord = new Map()
+    let diffMap = new Map()
+    if(greenArrays){
+        greenArrays = greenArrays[0]
+///Adds values to hashmap for correct placements
         for(let i = 0; i < 5; i++){
-
-            if(colorArrays[i] === 0){
-                // document.getElementById('s1').style.backgroundColor = "green"
-                individualArray.push(i)
-            }
-            if(colorArrays[i] === 1){
-                // document.getElementById('s2').style.backgroundColor = "green"
-                individualArray.push(i)
-
-            }
-            if(colorArrays[i] === 2){
-                // document.getElementById('s3').style.backgroundColor = "green"
-                individualArray.push(i)
-
-            }
-            if(colorArrays[i] === 3){
-                // document.getElementById('s4').style.backgroundColor = "green"
-                individualArray.push(i)
-
-            }
-            if(colorArrays[i] === 4){
-                // document.getElementById('s5').style.backgroundColor = "green"
-                individualArray.push(i)
-
-            }
+            diffMap.set(greenArrays[i], greenArrays[i])
         }       
-        console.log(individualArray) 
-    }
 
+        for(let i = 0; i < 5; i++){
+            for(let j = 0; j < 5; j++){
+                if(previousTries?.substring(i,i+1) == currentWord.substring(j,j+1)){
+                    yellowWord.set(i,i)
+                    wrongLetters.push(previousTries?.substring(i,i+1))
+                }
+            }
+        }
+    }
+    console.log(wrongLetters)
 
     return(
-        <div className='word-row'>
-            {individualArray[0] === 0 ? <div id="s1" className='green-square individual-letter-square'>{previousTries?.substring(0,1)}</div> : <div id="s1" className='individual-letter-square'>{previousTries?.substring(0,1)}</div> }
-            {individualArray[1] === 1 ? <div id="s2" className='green-square individual-letter-square'>{previousTries?.substring(1,2)}</div> : <div id="s2" className='individual-letter-square'>{previousTries?.substring(1,2)}</div> }
-            {individualArray[2] === 2 ? <div id="s3" className='green-square individual-letter-square'>{previousTries?.substring(2,3)}</div> : <div id="s3" className='individual-letter-square'>{previousTries?.substring(2,3)}</div> }
-            {individualArray[3] === 3 ? <div id="s4" className='green-square individual-letter-square'>{previousTries?.substring(3,4)}</div> : <div id="s4" className='individual-letter-square'>{previousTries?.substring(3,4)}</div> }
-            {individualArray[4] === 4 ? <div id="s5" className='green-square individual-letter-square'>{previousTries?.substring(4,5)}</div> : <div id="s5" className='individual-letter-square'>{previousTries?.substring(4,5)}</div> }
+        <div className="word-row">
+        {(() => {
+            if(diffMap.has(0)){
+                return(<div id="s1" className='green-square individual-letter-square'>{previousTries?.substring(0,1)}</div>)
+            }else if(yellowWord.has(0)){
+                return(<div id="s1" className='yellow-square individual-letter-square'>{previousTries?.substring(0,1)}</div>)
+            }else{
+                return (<div id="s1" className='gray-square individual-letter-square'>{previousTries?.substring(0,1)}</div>)
+            }    
+        
+        })()
+        }
+
+        {/* Square 2 */}
+        {(() => {
+            if(diffMap.has(1)){
+                return(<div id="s2" className='green-square individual-letter-square'>{previousTries?.substring(1,2)}</div>)
+            }else if(yellowWord.has(1)){
+                return(<div id="s2" className='yellow-square individual-letter-square'>{previousTries?.substring(1,2)}</div>)
+            }else{
+                return (<div id="s2" className='gray-square individual-letter-square'>{previousTries?.substring(1,2)}</div>)
+            }   
+            
+            })()
+            }
+
+        {/* Square 3 */}
+        {(() => {
+            if(diffMap.has(2)){
+                return(<div id="s3" className='green-square individual-letter-square'>{previousTries?.substring(2,3)}</div>)
+            }else if(yellowWord.has(2)){
+                return(<div id="s3" className='yellow-square individual-letter-square'>{previousTries?.substring(2,3)}</div>)
+            }else{
+                return (<div id="s3" className='gray-square individual-letter-square'>{previousTries?.substring(2,3)}</div>)
+            }       
+            
+            })()
+            }
+
+        {/* Sqaure 4 */}
+        {(() => {
+            if(diffMap.has(3)){
+                return(<div id="s4" className='green-square individual-letter-square'>{previousTries?.substring(3,4)}</div>)
+            }else if(yellowWord.has(3)){
+                return(<div id="s4" className='yellow-square individual-letter-square'>{previousTries?.substring(3,4)}</div>)
+            }else{
+                return (<div id="s4" className='gray-square individual-letter-square'>{previousTries?.substring(3,4)}</div>)
+            }  
+        
+        })()
+        }
+
+        {/* Sqaure 5 */}
+        {(() => {
+            if(diffMap.has(4)){
+                return(<div id="s5" className='green-square individual-letter-square'>{previousTries?.substring(4,5)}</div>)
+            }else if(yellowWord.has(4)){
+                return(<div id="s5" className='yellow-square individual-letter-square'>{previousTries?.substring(4,5)}</div>)
+            }else{
+                return (<div id="s5" className='gray-square individual-letter-square'>{previousTries?.substring(4,5)}</div>)
+            }   
+        
+        })()
+        }
+
         </div>
     )
+
+    
+    // return(
+    //     <div className='word-row'>
+    //         {diffMap.has(0) ? <div id="s1" className='green-square individual-letter-square'>{previousTries?.substring(0,1)}</div> : <div id="s1" className='gray-square individual-letter-square'>{previousTries?.substring(0,1)}</div> }
+            // {diffMap.has(1) ? <div id="s2" className='green-square individual-letter-square'>{previousTries?.substring(1,2)}</div> : <div id="s2" className='gray-square individual-letter-square'>{previousTries?.substring(1,2)}</div> }
+            // {diffMap.has(2) ? <div id="s3" className='green-square individual-letter-square'>{previousTries?.substring(2,3)}</div> : <div id="s3" className='gray-square individual-letter-square'>{previousTries?.substring(2,3)}</div> }
+            // {diffMap.has(3) ? <div id="s4" className='green-square individual-letter-square'>{previousTries?.substring(3,4)}</div> : <div id="s4" className='gray-square individual-letter-square'>{previousTries?.substring(3,4)}</div> }
+            // {diffMap.has(4) ? <div id="s5" className='green-square individual-letter-square'>{previousTries?.substring(4,5)}</div> : <div id="s5" className='gray-square individual-letter-square'>{previousTries?.substring(4,5)}</div> }
+    //     </div>
+    // )
 }
+
+
